1928d87e7fa13e5fcc7d1fd798a7e90deee4b73a
Albert Lombarte <alombarte@gmail.com>
2017-03-28 13:14:29 +0200

    Changed alias to create images from dotfile

--- utils/dot.sh
+++ utils/dot.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
-INPUT=$1
-OUTPUT=$1.png
+#!/bin/sh
 
-dot -Tpng $INPUT -o $OUTPUT
-open $OUTPUT
+IN="$1"
+OUT="$1.png"
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 input.dot"
+	exit 1
+fi
+dot -Tpng "$IN" -o "$OUT"
+open "$OUT"
