7022a511ad4009a08693c3f07e7e4da424b7ba57
Albert Lombarte <albert@lombarte.org>
2017-09-28 09:53:18 +0200

    Whiteboard cleaner: Accept multiple files to convert

--- utils/whiteBoardClean.sh
+++ utils/whiteBoardClean.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# https://gist.github.com/lelandbatey/8677901
-convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 "$2"
+# Usage: Call the script passing the files or pattern you want to convert
+
+for file in $*
+do
+ # do something on $file
+	if [ -f "$file" ]; then
+		file_output="clean_$file"
+	 	echo "Converting file $file to $file_output"
+		#https://gist.github.com/lelandbatey/8677901
+		convert "$file" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 "$file_output"
+	fi
+	done
