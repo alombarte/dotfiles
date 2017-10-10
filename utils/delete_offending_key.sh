91b1890ced3651934124409ccbda97d69d12abed
Albert Lombarte <albert@lombarte.org>
2017-10-10 11:50:09 +0200

    Alias to delete offending keys in known_hosts

---
+++ utils/delete_offending_key.sh
@@ -0,0 +1,13 @@
+NUM=$1
+re='^[0-9]+$'
+known_hosts="${HOME}/.ssh/known_hosts"
+
+if ! [[ $NUM =~ $re ]] ; then
+   echo "Specify the line number of the offending key" >&2; exit 1
+fi
+
+echo "Deleting key:"
+sed "${NUM}q;d" "$known_hosts"
+sed -if "${NUM}d" "$known_hosts"
+echo "Entries left: "
+cat $known_hosts | wc -l
