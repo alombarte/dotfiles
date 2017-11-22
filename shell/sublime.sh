78dd577464381b1bdd2beb4619d78f5f86b2d3e0
Albert Lombarte <albert@lombarte.org>
2017-11-22 21:29:18 +0100

    Sublime Text 3 installation

---
+++ shell/sublime.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+git clone https://github.com/Gakai/TOML
+git clone https://github.com/DisposaBoy/GoSublime
+git clone git@github.com:kkga/spacegray.git "Theme - Spacegray"
+git clone git@github.com:alombarte/sublime-user-preferences.git User
