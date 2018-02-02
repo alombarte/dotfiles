0f4f5e4c3c6204dc5d86d3bc9b1f122077ca8b77
Albert <alombarte@gmail.com>
2018-02-02 12:39:44 +0100

    Add TODO plugin in Sublime

--- shell/sublime.sh
+++ shell/sublime.sh
@@ -5,3 +5,4 @@ git clone https://github.com/Gakai/TOML
 git clone https://github.com/DisposaBoy/GoSublime
 git clone git@github.com:kkga/spacegray.git "Theme - Spacegray"
 git clone git@github.com:alombarte/sublime-user-preferences.git User
+git clone https://github.com/aziz/PlainTasks.git
