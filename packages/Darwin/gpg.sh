2e97968e50efef3a071d0b65b1c03578df902326
Albert Lombarte <alombarte@gmail.com>
2019-12-09 16:16:59 +0100

    Add GPG

--- packages/Darwin/gpg.sh
+++ packages/Darwin/gpg.sh
@@ -2,5 +2,5 @@
 brew upgrade gnupg  # This has a make step which takes a while
 brew link --overwrite gnupg
 brew install pinentry-mac
-echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
+echo "pinentry-program /usr/local/bin/pinentry-mac" > ~/.gnupg/gpg-agent.conf
 killall gpg-agent
