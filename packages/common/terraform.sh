59b1e9946ea2b32c193a68d0e10d140a302f7cdf
Albert Lombarte <alombarte@gmail.com>
2020-10-11 17:39:06 +0200

    Terraform installation is not using the right platform

--- packages/common/terraform.sh
+++ packages/common/terraform.sh
@@ -2,7 +2,7 @@
 # Manual installation of terraform (packages are way behind)
 
 VERSION=0.12.16
-PLATFORM=${uname | tr '[:upper:]' '[:lower:]'}
+PLATFORM="$(uname | tr '[:upper:]' '[:lower:]')"
 echo "What version of terraform do you wish to install?"
 echo "See https://releases.hashicorp.com/terraform/"
 read -p "Version: [${VERSION}] " target
@@ -14,7 +14,7 @@ read -p "Proceed with $target? (Y/N): " confirm && [[ $confirm == [yY] ]] || exi
 
 
 wget https://releases.hashicorp.com/terraform/${target}/terraform_${target}_${PLATFORM}_amd64.zip
-unzip terraform_${target}_linux_amd64.zip
-rm terraform_${target}_linux_amd64.zip
+unzip terraform_${target}_${PLATFORM}_amd64.zip
+rm terraform_${target}_${PLATFORM}_amd64.zip
 sudo mv terraform /usr/local/bin
 terraform -v
