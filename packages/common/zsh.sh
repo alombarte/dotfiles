335535eb173bf3e2e2760d4408ad0a3725ce7cd0
Albert Lombarte <alombarte@gmail.com>
2020-11-13 20:19:20 +0100

    Add OBS

--- packages/common/zsh.sh
+++ packages/common/zsh.sh
@@ -9,5 +9,3 @@ if [ ! -d "$HOME/.zsh" ]; then
 	git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 fi
-
-
