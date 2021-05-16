36266d98356e3194099073ee3bf8e1b14357977d
Albert Lombarte <alombarte@gmail.com>
2021-05-16 14:28:20 +0200

    Correct Ubuntu distro

--- packages/Linux/docker.sh
+++ packages/Linux/docker.sh
@@ -3,8 +3,7 @@
 if test ! "$(type docker)"; then
     sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt update -y
     apt-cache policy docker-ce
     sudo apt install docker-ce docker-compose -y
