40a64e00b87068ff46a05d058addf7d991698547
Albert Lombarte <albert@krakend.io>
2023-12-11 10:41:29 +0100

    Replace fonts source

--- packages/common/fonts.sh
+++ packages/common/fonts.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
-TMP_FONTS_DIR=/tmp/powerline-fonts
+TMP_FONTS_DIR=/tmp/nerd-fonts
+mkdir -p "$TMP_FONTS_DIR"
 
-if test ! "$(fc-list | grep Powerline)"; then
-	echo "Installing Powerline fonts (https://github.com/powerline/fonts)"
-	git clone https://github.com/powerline/fonts.git ${TMP_FONTS_DIR}
-	bash ${TMP_FONTS_DIR}/install.sh
-	rm -fr ${TMP_FONTS_DIR}
+if [[ $(uname) == 'Darwin' ]]; then
+  # MacOS
+  usr_share_dir="$HOME/Library/Fonts"
 else
-	echo "Powerline patched fonts already installed"
+  # Linux
+  usr_share_dir="$HOME/.local/share/fonts"
+  if [ -n "${XDG_DATA_HOME}" ]; then
+   usr_share_dir="${XDG_DATA_HOME}/fonts"
+  fi
 fi
+usr_font_dir="${usr_share_dir}/NerdFonts"
+mkdir -p "$usr_font_dir"
+
+cd "$TMP_FONTS_DIR" || exit 1
+
+fonts=(
+    VictorMono
+    Meslo
+    Ubuntu
+    Inconsolata
+    )
+for i in "${fonts[@]}"; do
+    if test ! "$(fc-list | grep $i)"; then
+        echo "Installing font $i"
+        curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$i.zip"
+        unzip -o "$i.zip"
+        rm "$i.zip"
+        echo "Copying font to $usr_font_dir"
+        mv * "$usr_font_dir/"
+    else
+        echo "Font $i already installed"
+    fi
+done
